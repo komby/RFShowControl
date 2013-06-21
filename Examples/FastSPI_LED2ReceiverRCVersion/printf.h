f0ea2d32bbe18ee16e930c0cf1375a8d8a4ef9f7
Mathew Mrosko <mmrosko@jetheaddev.com>
2013-06-20 21:19:34 -0700

    Make the FastSPI printf the same as the others.

--- Examples/FastSPI_LED2ReceiverRCVersion/printf.h
+++ Examples/FastSPI_LED2ReceiverRCVersion/printf.h
@@ -1,11 +1,11 @@
 /*
  Copyright (C) 2011 J. Coliz <maniacbug@ymail.com>
- 
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  version 2 as published by the Free Software Foundation.
  */
- 
+
 /**
  * @file printf.h
  *
@@ -16,16 +16,22 @@
 #ifndef __PRINTF_H__
 #define __PRINTF_H__
 
-int serial_putc( char c, FILE * ) 
+#ifdef ARDUINO
+
+int serial_putc( char c, FILE * )
 {
   Serial.write( c );
 
   return c;
-} 
+}
 
 void printf_begin(void)
 {
   fdevopen( &serial_putc, 0 );
 }
+
+#else
+#error This example is only for use on Arduino.
+#endif // ARDUINO
 
 #endif // __PRINTF_H__
