0233eb039ea48c33bfb0d9af0d4fd8d3c96b5460
Greg Scull <komby@komby.com>
2013-07-06 22:38:41 -0700

    draft of OTA configuration
    Really this is  just a proof of concept.  Configuring two simple
    receivers.

---
+++ examples/RFPixelControlConfiguration/RFPixelControlConfiguration.ino
@@ -0,0 +1,150 @@
+#include <util.h>
+
+#include <RFPixelControl.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include "printf.h"
+#include <OTAConfig.h>
+
+#include <Arduino.h>
+
+#include <IPixelControl.h>
+
+
+/*
+
+*  Created on: June 30 2013
+*      Author: Greg Scull
+*/
+
+#define RF_DELAY 2000
+
+
+/***************************  CONFIGURATION SECTION *************************************************/
+
+
+//Setup  a RF pixel control
+//RF1 v.01 board uses Radio 9,10
+//ARDUINO ETHERENET NEEDS PIN 10 Relocate CSN to pin8
+//RFPixelControl radio(9,8);
+
+RFPixelControl radio(8,7);
+//kombyone due transmitter board radio settings.
+//RFPixelControl radio(33,10);
+
+
+
+/***************************  CONFIGURATION SECTION *************************************************/
+
+//change the mac (0xEF, 0xE0, 0xE1, ... 0xE9)
+byte mac[] = {
+0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+//Change this IP to one on your network
+//IPAddress ip(192, 168, 2, 76);
+
+
+
+// Radio pipe addresses for the 2 nodes to communicate.
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+
+
+char packetBuffer[700]; //buffer to hold incoming packet,
+byte str[32];
+
+
+void setup(){
+
+	Serial.begin(57600);
+	
+	printf_begin();
+	//radio.Initalize( radio.TRANSMITTER, pipes, TRANSMIT_CHANNEL );
+       radio.Initalize( radio.TRANSMITTER, pipes, RF_NODE_CONFIGURATION_CHANNEL, RF24_1MBPS ,1);
+	delayMicroseconds(5000);
+
+	radio.printDetails();
+}
+int maxprint=0;
+int len=0;
+int freecount=0;
+long duration = millis();
+long durationAfter = 0;
+long checkduration = 0;
+boolean status;
+void loop () {
+       
+     //FirstPacket
+     //ConfigurationDataForReceiver 1
+     //
+     //COntroller 1  ID 50
+     int logicalSequence = 0;
+      str[IDX_CONTROLLER_ID]  = 50;
+     //The First Configuration Packet contains the number of logical controllers for a given controller
+      str[IDX_CONFIG_PACKET_TYPE] = CONTROLLERINFOINIT;
+      str[IDX_RF_LISTEN_CHANNEL] = 100;
+      str[IDX_RF_LISTEN_RATE] = RF24_1MBPS;
+      str[IDX_NUMBER_OF_LOGICAL_CONTROLLERS] = 1;
+       printf("WritingPayloadFor init packet\n");
+       radio.write_payload( &str[0], 32 );
+       delayMicroseconds(5000);
+       status = radio.get_status();
+       while (status & 0x01) {
+          status = radio.get_status();
+      }
+      
+      
+      str[IDX_CONFIG_PACKET_TYPE] =LOGICALCONTROLLER_LED ;
+      str[IDX_CONTROLLER_ID] = 50;
+      str[IDX_LOGICAL_CONTROLLER_NUMBER] = logicalSequence;
+      str[IDX_LOGICAL_CONTROLLER_NUM_CHANNELS] = 3;
+      str[IDX_LOGICAL_CONTROLLER_START_CHANNEL] = 1;
+      printf("WritingPayloadFor config packet\n");	
+     
+       radio.write_payload( &str[0], 32 );
+       delayMicroseconds(5000);
+       status = radio.get_status();
+       while (status & 0x01) {
+          status = radio.get_status();
+      }
+       //FirstPacket
+     //ConfigurationDataForReceiver 1
+     //
+     //COntroller 1  ID 33
+      str[IDX_CONTROLLER_ID]  = 33;
+      logicalSequence = 0;
+     //The First Configuration Packet contains the number of logical controllers for a given controller
+      str[IDX_CONFIG_PACKET_TYPE] = CONTROLLERINFOINIT;
+      str[IDX_RF_LISTEN_CHANNEL] = 100;
+      str[IDX_RF_LISTEN_RATE] = RF24_1MBPS;
+      str[IDX_NUMBER_OF_LOGICAL_CONTROLLERS] = 1;
+       printf("WritingPayloadFor init packet\n");
+       radio.write_payload( &str[0], 32 );
+       delayMicroseconds(5000);
+       status = radio.get_status();
+       while (status & 0x01) {
+          status = radio.get_status();
+      }
+      
+      
+      str[IDX_CONFIG_PACKET_TYPE] = LOGICALCONTROLLER_LED;
+      str[IDX_CONTROLLER_ID] = 33;
+      str[IDX_LOGICAL_CONTROLLER_NUMBER] = logicalSequence;
+      str[IDX_LOGICAL_CONTROLLER_NUM_CHANNELS] = 150;
+      str[IDX_LOGICAL_CONTROLLER_START_CHANNEL] = 4;
+
+
+	 printf("WritingPayloadFor config packet\n");	
+     
+       radio.write_payload( &str[0], 32 );
+       delayMicroseconds(5000);
+       status = radio.get_status();
+       while (status & 0x01) {
+          status = radio.get_status();
+      }
+      
+      
+      
+}
+
+
