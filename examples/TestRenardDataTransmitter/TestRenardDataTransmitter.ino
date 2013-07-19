b8d05842935b24431c17b5a560d5eb93a565c895
Mathew Mrosko <rfpixelcontrol@matmrosko.com>
2013-07-18 20:20:09 -0700

    Updates to the renard transmitter to get it working.  Only tested to the coded 21 channels.

--- examples/TestRenardDataTransmitter/TestRenardDataTransmitter.ino
+++ examples/TestRenardDataTransmitter/TestRenardDataTransmitter.ino
@@ -17,11 +17,6 @@
 #include "printf.h"
 
 /***************************  CONFIGURATION SECTION *************************************************/
-
-//How many pixels do you want to transmit data for
-//#define NUM_CHANNELS 64
-#define RF_DELAY      2000
-
 //use channel 100
 #define TRANSMIT_CHANNEL 100
 
@@ -32,80 +27,96 @@ RFPixelControl radio(8,7);
 //RFPixelControl radio(33,10);
 
 
-#define RENARD_SERIAL_BAUD 57600
+#define RENARD_SERIAL_BAUD	57600
+//How many pixels do you want to transmit data for
+#define NUM_CHANNELS		21
+/***************************  CONFIGURATION SECTION *************************************************/
+
+#define RF_DELAY      2000
+
 #define PAD 0x7D
 #define SYNC 0x7E
 #define ESCAPE 0x7F
 #define COMMAND 0x80
-/***************************  CONFIGURATION SECTION *************************************************/
 
 bool sync;
+bool status;
 
 // Radio pipe addresses for the 2 nodes to communicate.
 const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
 
- int numberOfChannels;
-
-byte buffer[512]; // tcp/ip send and receive buffe
-uint8_t dataMap [255];
 byte packetData[32];
 
-void setup(){
-	Serial.begin(57600);
-	Serial.println("\n[RenardTransmitter test]\n");
-          printf_begin();
-        radio.Initalize( radio.TRANSMITTER, pipes, TRANSMIT_CHANNEL );
-	delayMicroseconds(150);
-        numberOfChannels=0;//start with no channels
-        radio.printDetails();
-        
-        //setup the datamap
-        for(int i=0;i<255;i++){
-          dataMap[i]=i;
-        }
+int renardRead( uint8_t *bytes, uint8_t byte_count );
+
+void print_data(char *data)
+{
+	printf("--%3d:", 0);
+	for ( int i = 0; i < NUM_CHANNELS; i++ )
+	{
+		printf(" 0x%02X", data[i] & 0xFF);
+		if ( (i-7) % 8 == 0 )
+		{
+			printf("\n--%3d:", i);
+		}
+	}
+	printf("\n");
 }
 
-  uint8_t packetCount = 0;
+void setup()
+{
+	Serial.begin(RENARD_SERIAL_BAUD);
+	Serial.println("\n[RenardTransmitter test]\n");
+    printf_begin();
+    radio.Initalize( radio.TRANSMITTER, pipes, TRANSMIT_CHANNEL, RF24_250KBPS, 50 );
+	delayMicroseconds(150);
+    radio.printDetails();
+}
+
+void loop ()
+{
   uint8_t bytes_read = 0;
-  bool startOver=true;
-  bool status;
-void loop () {
-   
-  uint8_t bytes_read;
-  bytes_read = renardRead(&packetData[0], 30);
-  //if we got less than expected,  fill the remainder with PADs for now 
-  if ( bytes_read < 30){
-    for(int j=bytes_read-1;j<30;j++){
-      packetData[j]=PAD;
-    }
+
+  for (int i = 0; i < NUM_CHANNELS; i += 30)
+  {
+	  bytes_read = renardRead(packetData, (NUM_CHANNELS-i >= 30 ? 30 : (NUM_CHANNELS%30) ));
+//	  print_data((char *)packetData);
+	  //if we got less than expected,  fill the remainder with PADs for now 
+	  if (bytes_read < 30)
+	  {
+	    for(int j=bytes_read;j<30;j++)
+		{
+	      packetData[j]=PAD;
+	    }
+	  }
+
+	//With the next line, the data doesn't appear to make it to the receiver?!?
+//	packetData[30]=bytes_read;
+	radio.write_payload( &packetData[0], 32 );
+	delayMicroseconds(RF_DELAY);
+	status = radio.get_status();
+	//With the following while loop, we get stuck forever after the first transmission
+//	while (status & 0x01)
+//	{
+//		status = radio.get_status();
+//	}
   }
-  
-  packetData[30]=packetCount;
-  radio.write_payload( &packetData[0], 32 );
-  delayMicroseconds(RF_DELAY);
-  status = radio.get_status();
-  while (status & 0x01) {
-    status = radio.get_status();
-  }
-  if (sync){
-    packetCount=0;
-    sync=false;
-  }
-} 
+}
 
 void wait_for_serial()
 {
     while ( ! Serial.available() > 0 ) { }
 }
- //thanks materdaddy
-int renardReadBytes( uint8_t *bytes, uint8_t bytes_size )
+
+//thanks materdaddy
+int renardReadBytes(uint8_t *bytes, uint8_t bytes_size)
 {
   int in_byte = 0;
   int bytes_read;
  
   for ( bytes_read = 0; bytes_read < bytes_size; )
   {
-    wait_for_serial();
+	wait_for_serial();
     in_byte = Serial.read();
  
     switch (in_byte)
@@ -118,8 +129,9 @@ int renardReadBytes( uint8_t *bytes, uint8_t bytes_size )
         continue;
  
       case(0x7F): // Escape character, we need to read one more byte to get our actual data
-        wait_for_serial();
+		wait_for_serial();
         in_byte = Serial.read();
+
         switch (in_byte)
           {
             case(0x2F): // renard wants an 0x7D
@@ -133,14 +145,11 @@ int renardReadBytes( uint8_t *bytes, uint8_t bytes_size )
  
     bytes[bytes_read++] = in_byte;
   }
-    return bytes_read;
+
+  return bytes_read;
 }
-  
-  
-  
-  
-  
-  int renardRead( uint8_t *bytes, uint8_t byte_count )
+
+int renardRead(uint8_t *bytes, uint8_t byte_count)
 {
   int in_byte = 0;
  
@@ -151,12 +160,12 @@ int renardReadBytes( uint8_t *bytes, uint8_t bytes_size )
     if ( in_byte == 0x7E ) // Sync byte signifies start of packet
       sync = true;
   }
- 
+
   if ( sync )
   {
     sync = false;
     wait_for_serial();
-    in_byte = Serial.read();
+	in_byte = Serial.read();
     if ( in_byte == 0x80 ) // Read from here
     {
       return renardReadBytes(bytes, byte_count);
