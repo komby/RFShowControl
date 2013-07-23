6259a1d757a16d708dbcdd4981efd7cffc7d91ec
Greg Scull <komby@komby.com>
2013-07-22 22:31:17 -0700

    added dumb rgb sketch draft

--- RFPixelControl.h
+++ RFPixelControl.h
@@ -1,13 +1,13 @@
 /*
- * RFPixelControl.h
- * This class is built to serve as an extension of the RF24 library for use in Pixel controllers
- * This code was created based on the RF ColorNode code by Joe Johnson
- * it has been refactored such that the RF24 core library is not needed
- * to be modified in order to be used.
- *
- *  Created on: Mar, 2013
- *      Author: Greg Scull - komby@komby.com
- */
+* RFPixelControl.h
+* This class is built to serve as an extension of the RF24 library for use in Pixel controllers
+* This code was created based on the RF ColorNode code by Joe Johnson
+* it has been refactored such that the RF24 core library is not needed
+* to be modified in order to be used.
+*
+*  Created on: Mar, 2013
+*      Author: Greg Scull - komby@komby.com
+*/
 
 #ifndef RFPIXELCONTROL_H_
 #define RFPIXELCONTROL_H_
@@ -30,7 +30,7 @@ typedef enum { role_transmitter = 1, role_receiver = 0} role_e;
 
 
 class RFPixelControl : public RF24 {
-public:
+	public:
 	//RFPixelAdapter();
 	virtual ~RFPixelControl();
 
@@ -38,106 +38,107 @@ public:
 	static const int  RECEIVER = 0;
 
 
-	 /**
-	   * Constructor
-	   *
-	   * Creates a new instance of this driver.  Before using, you create an instance
-	   * and send in the unique pins that this chip is connected to.
-	   *
-	   * @param _cepin The pin attached to Chip Enable on the RF module
-	   * @param _cspin The pin attached to Chip Select
+	/**
+	* Constructor
+	
+	*
+	* Creates a new instance of this driver.  Before using, you create an instance
+	* and send in the unique pins that this chip is connected to.
+	*
+	* @param _cepin The pin attached to Chip Enable on the RF module
+	* @param _cspin The pin attached to Chip Select
 
-	   */
+	*/
 	RFPixelControl(uint8_t _cepin, uint8_t _cspin) ;
 
 
 	/**
-	 * Setup the radio as a transmitter or Receiver.
-	 * The values for this are stored in
-	 * the TRANSMITTER and RECEIVER values.
-	 */
+	* Setup the radio as a transmitter or Receiver.
+	* The values for this are stored in
+	* the TRANSMITTER and RECEIVER values.
+	*/
 	bool Initalize( int  role, const uint64_t * pPipes, int pChannel, rf24_datarate_e pDataRate, int pControllerId);
 
-void SetStartAndEndChannels();
+	void SetStartAndEndChannels();
 
 	
 	/**
-	 * Check what channel the radio is listeneing on
-	 */
-	 uint8_t GetChannel(void);
-	 
+	* Check what channel the radio is listening on
+	*/
+	uint8_t GetChannel(void);
+	
 	/**
-	  * overriding method from base class
-	  *
-	  * Based on code suggestion from Joe
-	  * overrides the base  SPI.setClockDivider(SPI_CLOCK_DIV4);
-	  * to the needed  SPI.setClockDivider(SPI_CLOCK_DIV2);
-	  *
-	  * @param mode HIGH to take this unit off the SPI bus, LOW to put it on
-	 */
-	 void  csn(int mode);
+	* overriding method from base class
+	*
+	* Based on code suggestion from Joe
+	* overrides the base  SPI.setClockDivider(SPI_CLOCK_DIV4);
+	* to the needed  SPI.setClockDivider(SPI_CLOCK_DIV2);
+	*
+	* @param mode HIGH to take this unit off the SPI bus, LOW to put it on
+	*/
+	void  csn(int mode);
 	/**
-	   * Write the transmit payload
-	   *
-	   * The size of data written is the fixed payload size, see getPayloadSize()
-	   *
-	   * @param buf Where to get the data
-	   * @param len Number of bytes to be sent
-	   * @return Current value of status register
-	   */
-	  uint8_t write_payload(const void* buf, uint8_t len);
-	  
-	  void DisplayDiagnosticStartup(IPixelControl * string);
-	  uint8_t get_status(void);
-	  
-	  /************************************************************************/
-	  /* Listen will be listening for all the data needed for an update        
-	  /* When it returns true its time to update all controllers
-	  /************************************************************************/
-	  bool Listen(void);
-	  
-	   int SaveConfigurationToEEPROM();
-	   
-	   
-	   int ReadConfigurationFromEEPROM();
-	   
-	   void PrintControllerConfig(ControllerInfo pControllerInfo);
-	   
-	   bool GetControllerDataUpdate( void );
-	   bool ProcessPacket(byte*  dest, byte* src);
-	   uint8_t* GetControllerDataBase( uint8_t pLogicalControllerNumber );
-	   /** 
-		ConfigureReceiverAtStartup 
-		Returns true if OTAConfig is received.
-	 */
-    bool ConfigureReceiverAtStartup(uint32_t pReceiverId);	
+	* Write the transmit payload
+	*
+	* The size of data written is the fixed payload size, see getPayloadSize()
+	*
+	* @param buf Where to get the data
+	* @param len Number of bytes to be sent
+	* @return Current value of status register
+	*/
+	uint8_t write_payload(const void* buf, uint8_t len);
+	
+	void DisplayDiagnosticStartup(IPixelControl * string);
+	uint8_t get_status(void);
+	
+	/************************************************************************/
+	/* Listen will be listening for all the data needed for an update
+	/* When it returns true its time to update all controllers
+	/************************************************************************/
+	bool Listen(void);
+	
+	int SaveConfigurationToEEPROM();
+	
+	
+	int ReadConfigurationFromEEPROM();
+	
+	void PrintControllerConfig(ControllerInfo pControllerInfo);
+	
+	bool GetControllerDataUpdate( void );
+	bool ProcessPacket(byte*  dest, byte* src);
+	uint8_t* GetControllerDataBase( uint8_t pLogicalControllerNumber );
+	/**
+	ConfigureReceiverAtStartup
+	Returns true if OTAConfig is received.
+	*/
+	bool ConfigureReceiverAtStartup(uint32_t pReceiverId);
 	
 	bool EnableOverTheAirConfiguration(uint8_t enabled);
 	
 	int GetNumberOfChannels(int pLogicalController);
 	void AddLogicalController(int pLControllerName, int pLcontrollerStartChannel, int pLControllerNumChannels, int pLControllerBaudRate);
 	
-	  void PrintControllerConfig(void);
+	void PrintControllerConfig(void);
 
-private:
-	  uint8_t csn_pin; /**< SPI Chip select redefined because private in base ---  its hacky*/
-      int _channel;  //channel to transmit on
-	  rf24_datarate_e _rf_data_rate; //Data Rate for the RF.
-      
-	  bool dataRateSuccess;
-	  bool payloadSizeSetSuccessful;
-	  bool channelSetSuccessfully;
-	  ControllerInfo * _controllers;
-	  uint8_t _numControllers;
-	  uint32_t _controllerId;
-	  byte packetData[RF_PACKET_LENGTH];
-	  uint8_t  channelData[512];
-	  int _startChannel;
-	  int _endChannel;
-	  bool _otaConfigEnable;
-	  
-	  
+	private:
+	uint8_t csn_pin; /**< SPI Chip select redefined because private in base ---  its hacky*/
+	int _channel;  //channel to transmit on
+	rf24_datarate_e _rf_data_rate; //Data Rate for the RF.
+	
+	bool dataRateSuccess;
+	bool payloadSizeSetSuccessful;
+	bool channelSetSuccessfully;
+	ControllerInfo * _controllers;
+	uint8_t _numControllers;
+	uint32_t _controllerId;
+	byte packetData[RF_PACKET_LENGTH];
+	uint8_t  channelData[512];
+	int _startChannel;
+	int _endChannel;
+	bool _otaConfigEnable;
+	
+	
 
-};
+	};
 
-#endif /* RFPIXELCONTROL_H_ */
+	#endif /* RFPIXELCONTROL_H_ */
---
+++ examples/RF1DumbRGB/RF1DumbRGB.ino
@@ -0,0 +1,153 @@
+/*
+ * FastSPI_LED2Receiver  RFPixel Control Receiver Sketch for handling the FAST_SPI2 Release candidate.
+ * 
+ *  Created on: Mar  2013
+ *  Updated 6/2/2013
+ *      Author: Greg Scull, komby@komby.com
+ *      
+ *      Users of this software agree to hold harmless the creators and contributors
+ *      of this software. You, by using this software, are assuming all legal responsibility
+ *      for the use of the software and any hardware it is used on.
+ *
+ * 
+ * Some terminology to help you out
+ * 
+ * There are two uses of the term Channel in this code.  
+ *
+ * 1.  RF Channel - This is the channel that the RF Radio is using to communicate with the transmitter
+ * 2.  DMX Channel - For a single universe of 512  this DMX Channel is one of those 512 channels.
+ *    
+ *    Note - Though I called it a DMX channel I want to point out that this code does not know anything about DMX.
+ *            This code only knows about channels in relationship to the packet position which they occur.
+ *
+ *    What Protocol is this listening to?
+ *
+ *    The nRF24L01+ uses a variable sized payload for packets.  In the RFPixelControl library we are using the maximum
+ *    of 32 Bytes.  These bytes are as follows
+ *    -------------------------------------------------------------------------------------------------------------------
+     | 1 | 2 | 3 | 4 | 5 | ...................................................................................| 31 | 32  |  
+ *    -------------------------------------------------------------------------------------------------------------------
+ *    Bytes 1-30 ( Index 0-29 ) Channel Data  Each byte represents 1 Channel of data,  One of the R , G, or B values.
+ *    Byte  31 ( Index 30 )  - Sequence number,  This represents which order the packets are being sent
+ *                             There will 18 Packets sent for an entire universe,  this index will contain 0-17.
+ *    Byte 32   (Unused) 
+ * 
+ *      
+ */
+
+
+#include <Arduino.h>
+#include <RFPixelControl.h>
+#include <IPixelControl.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include "printf.h"
+
+
+//#define RENARD_SERIAL_BAUD 57600
+//#define PAD 0x7D
+//#define SYNC 0x7E
+//#define ESCAPE 0x7F
+//#define COMMAND 0x80
+#define REDPIN 10
+#define GREENPIN 5
+#define BLUEPIN 9
+ 
+ 
+ //Step 2.
+//What Pins is your nRF24L01+ CE and CSN connected to?  
+//Usage:   RFPixelControl radio(CE, CSN);
+
+//RF1 v0.2 & RF1_12V 0.1 PCB ises( PB0, Arduino Pin 8  for CE ) (PD7 Arduino pin 7 for CSN)
+RFPixelControl radio(8,7);
+//RFPixelControl radio(9,10); //RF1 Version 0.1 (maniacbug wiring)
+
+
+ //to use OTA configuration
+#define  OVER_THE_AIR_CONFIG_ENABLE 1
+#define RECEIVER_UNIQUE_ID 50  //make this different for each receiver.
+ 
+#define NUM_CHANNELS 3  
+#define START_CHANNEL 1 //Where in the universe do we start
+#define FINAL_CHANNEL 3 //DO Refactory out, addition would be easy....
+
+#define DATA_BUFFER_LENGTH  3  //TODO Refactor so this is just the NUM_CHANNELS
+
+
+#define LISTEN_CHANNEL 100 // the channel for the RF Radio
+
+
+
+
+bool readytoupdate=false;
+
+byte * buffer;
+
+ 
+ 
+/**************END CONFIGURATION SECTION ***************************/
+
+// Radio pipe addresses for the 2 nodes to communicate.
+//these should not need to be modified
+const uint64_t pipes[2] = { 
+  0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+//Uncomment for serial
+#define DEBUG 0
+
+
+//Arduino setup function.
+void setup() {
+  Serial.begin(57600);
+   //  if (NUMBER_OF_DUMB_RGB_ADDON_CHANNELS){
+         pinMode(REDPIN, OUTPUT);
+         pinMode(GREENPIN, OUTPUT);
+          pinMode(BLUEPIN, OUTPUT);
+          
+ buffer[0]=255;
+    // }
+    
+    radio.EnableOverTheAirConfiguration(OVER_THE_AIR_CONFIG_ENABLE);
+   if(!OVER_THE_AIR_CONFIG_ENABLE)
+   {
+         int logicalControllerSequenceNum = 0;
+         radio.AddLogicalController(logicalControllerSequenceNum, START_CHANNEL, NUM_CHANNELS,0);
+        
+   }
+   printf_begin();
+	
+   	delay(2000);
+
+      radio.Initalize( radio.RECEIVER, pipes, LISTEN_CHANNEL, RF24_1MBPS ,50);
+      radio.printDetails(); 
+      //initalize data buffer
+      buffer= radio.GetControllerDataBase(0);
+
+        delay (200);
+
+  
+        dumbRGBShow(255, 0, 0, 25);
+         (2000);
+        dumbRGBShow(0, 255, 0, 25);
+        delay (200);
+        dumbRGBShow(0, 0, 255, 25);
+        delay (200);
+   dumbRGBShow(0, 0, 0, 25);
+
+}
+
+//RF Listening to DMX packets loop
+void loop(void){
+   if( radio.Listen()){
+     dumbRGBShow(buffer[0], buffer[1], buffer[2],1);
+   }
+    
+  }
+ 
+
+void dumbRGBShow( int r, int g, int b , int d) {
+  analogWrite(REDPIN, r);
+  analogWrite(GREENPIN, g);
+  analogWrite(BLUEPIN, b);
+ delay(d);
+}
