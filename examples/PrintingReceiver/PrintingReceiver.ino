70e6428875c211aa7e0db74c36d909c55d3becd1
Greg Scull <komby@komby.com>
2013-07-28 19:55:15 -0700

    Fixed Compile errors on PrintingReceiver

--- examples/PrintingReceiver/PrintingReceiver.ino
+++ examples/PrintingReceiver/PrintingReceiver.ino
@@ -1,11 +1,11 @@
 /*
- * 
- *
- *  Created on: Mar  2013
- *      Author: Greg Scull, komby@komby.com
- *
- *   Print packet content and optionally packet data snooping on transmitter channels
- */
+*
+*
+*  Created on: Mar  2013
+*      Author: Greg Scull, komby@komby.com
+*
+*   Print packet content and optionally packet data snooping on transmitter channels
+*/
 
 
 #include <Arduino.h>
@@ -17,12 +17,13 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include "printf.h"
-/**
- 
- */
+
 
 //Uncomment for serial output of all data
 #define FULL_PRINT_DEBUG 0
+
+//Make sure you choose a unique ID if you are using OTA config!
+#define RECEIVER_UNIQUE_ID 33
 
 
 //NRF24L01+ Items
@@ -52,54 +53,49 @@ byte gotstr[32];
 //Include this after all configuration variables are set
 #include <RFPixelControlConfig.h>
 
-
-
-// Radio pipe addresses for the 2 nodes to communicate.
-const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-
 byte data[32];
 
-//Arduino setup function.
+
 void setup() {
- printf_begin();
- 
-  radio.EnableOverTheAirConfiguration(OVER_THE_AIR_CONFIG_ENABLE);
-	 
-	 
-	 if(!OVER_THE_AIR_CONFIG_ENABLE)
-	 {
+	printf_begin();
+	
+	radio.EnableOverTheAirConfiguration(OVER_THE_AIR_CONFIG_ENABLE);
+	
+	
+	if(!OVER_THE_AIR_CONFIG_ENABLE)
+	{
 		
-		 radio.AddLogicalController(0, 1, 464,  0);
-	 }
+		radio.AddLogicalController(0, 1, 464,  0);
+	}
 	Serial.begin(57600);
 	Serial.write("Initializing DEBUG listener\n");
 
 	radio.Initialize( radio.RECEIVER, pipes, LISTEN_CHANNEL,DATA_RATE ,RECEIVER_UNIQUE_ID);
-  radio.printDetails(); 
+	radio.printDetails();
 	Serial.write("Init and Paint LEDS for startup \n");
 
 }
-     
+
 
 
 
 //RF Listening to DMX packets loop
 void loop(void){
-  int packetOffset = 0;
-  if ( radio.available() )
-  {
-     radio.read( &data, 32 );
-    
-        packetOffset =  data[30] *30;
-        printf("\n\r--%3d:",  data[30] );
-        for ( int i =0;i<30 && FULL_PRINT_DEBUG;i++)
-        {
-          printf(" 0x%02X", data[i]);
-  	  if ( (i-7) % 8 == 0 )
-	  {
-	    printf("\n\r--%3d:", i  + packetOffset);
-	  }
-        }
-     
-  }
+	int packetOffset = 0;
+	if ( radio.available() )
+	{
+		radio.read( &data, 32 );
+		
+		packetOffset =  data[30] *30;
+		printf("\n\r--%3d:",  data[30] );
+		for ( int i =0;i<30 && FULL_PRINT_DEBUG;i++)
+		{
+			printf(" 0x%02X", data[i]);
+			if ( (i-7) % 8 == 0 )
+			{
+				printf("\n\r--%3d:", i  + packetOffset);
+			}
+		}
+		
+	}
 }
